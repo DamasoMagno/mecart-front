09f0ba4f27417355898ade93b62600c2e0d8e767
DamasoMagno <limamdamaso@gmail.com>
2023-05-26 22:39:58 -0300

    Manifest moved to vite.config

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { registerSW } from "virtual:pwa-register";
+
+const updateSM = registerSW({
+  onNeedRefresh() {
+    let confirmUpdate = confirm("New content available. Reload?");
+
+    if (confirmUpdate) {
+      updateSM(true);
+    }
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,52 @@
-import { defineConfig } from 'vite'
-import { VitePWA } from "vite-plugin-pwa"
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ 
+    VitePWA({
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      manifest: {
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        background_color: "#202024",
+        name: "MeCart",
+        short_name: "MeCart",
+        description: "Monte seu carrinho de compras com consciência.",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      includeAssets: ["**/*"],
       registerType: "autoUpdate",
       devOptions: {
         enabled: true,
-        type: "module"
-      }
-    })
-  ]
-})
+        type: "module",
+      },
+    }),
+  ],
+});
